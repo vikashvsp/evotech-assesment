4374490be6101e144c8d06199bd5a01eff383dd6
vikash kumar <vkvikashkumar987@gmail.com>
2023-12-22 22:22:30 +0530

    sidebar skeleton done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import MainContent from "./components/mainComponent/MainContent";
+import Sidebar from "./components/sideBar/sidebar";
+
+
+
+function App() {
+  return (
+    <div>
+      <Sidebar />
+      <MainContent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/mainComponent/MainContent.js
@@ -0,0 +1,14 @@
+// src/components/MainContent.js
+import React from 'react';
+import './MainContent.css';
+
+const MainContent = () => {
+     return (
+          <div className="main-content">
+               {/* Main content goes here */}
+               Main Content
+          </div>
+     );
+};
+
+export default MainContent;
